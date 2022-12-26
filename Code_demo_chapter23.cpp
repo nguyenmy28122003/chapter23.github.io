bf1173fc834569a3213b898e624cb872a2e1edbc
nguyenmy28122003 <98101094+nguyenmy28122003@users.noreply.github.com>
2022-12-26 22:51:32 +0700

    Add files via upload

---
+++ Code_demo_chapter23.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <float.h>
+
+#pragma fenv_access(on)
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    float a = 1.75f, b = 1e-6f;
+
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(10);
+
+    cout << "a = " << a << ", b = " << b << endl;
+
+    _controlfp_s(NULL, _RC_DOWN, _MCW_RC);
+
+    cout << "Result of a + b rounded down: " << a + b << endl;
+    cout << "Result of a - b rounded down: " << a - b << endl;
+
+    _controlfp_s(NULL, _RC_UP, _MCW_RC);
+    cout << "Result of a + b rounded up: " << a + b << endl;
+    cout << "Result of a - b rounded up: " << a - b << endl;
+
+    _controlfp_s(NULL, _RC_NEAR, _MCW_RC);
+    cout << "Result of a + b rounded to nearest: " << a + b << endl;
+    cout << "Result of a - b rounded to nearest: " << a - b << endl;
+
+    return 0;
+}
